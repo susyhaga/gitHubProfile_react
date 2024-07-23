1d6fc15a0ae4ca5fa9001aa7bc3a63b6eb948aef
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2024-07-23 17:32:43 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,34 @@ import RepoList from './components/ReposList'
 
 function App() {
   const [userName, setUserName] = useState('');
+  const [isFullScreen, setIsFullScreen] = useState(true);
+
+  const handleInputBlur = (e) => {
+    setUserName(e.target.value);
+    if (e.target.value.length > 4) {
+      setIsFullScreen(false);
+    } else {
+      setIsFullScreen(true);
+    }
+  };
+
   return (
     <>
-      <div className='inputContainer'>
+      <div className={`inputContainer ${isFullScreen ? 'fullscreen' : 'shrink'}`}>
         <h1 className='title'>GitHub Profile</h1>
         <p className='p'>Type a username GitHub</p>
-        <input className='input' type="text" onBlur={(e)=>setUserName(e.target.value)}/>
-       </div>
-        {userName.length>4 &&(
-      
-            <Perfil userName={userName} className=''></Perfil>
-            <RepoList userName={userName}></RepoList>
-  
+        <input className='input' type="text" onBlur={handleInputBlur} />
+      </div>
+      <div className='contentContainer'>
+        {userName.length > 4 && (
+          <>
+            <Perfil userName={userName} className='' />
+            <RepoList userName={userName} />
+          </>
         )}
+      </div>
     </>
-  )
+  );
 }
 
 export default App
