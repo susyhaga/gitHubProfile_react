f2cd205b1edc7e66cbdd9126c6a0ff19f6261969
Susy Haga <71126492+susyhaga@users.noreply.github.com>
2024-07-23 16:49:00 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,12 @@ function App() {
         <h1 className='title'>GitHub Profile</h1>
         <p className='p'>Type a username GitHub</p>
         <input className='input' type="text" onBlur={(e)=>setUserName(e.target.value)}/>
-      </div>
         {userName.length>4 &&(
-          <>
+      
             <Perfil userName={userName} className=''></Perfil>
+       </div>
             <RepoList userName={userName}></RepoList>
-          </>
+  
         )}
     </>
   )
